6b2e6a62600a6a416aaa39f1406e3a7744863f1d
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2025-03-12 17:17:02 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req,res)=>{
+    res.json({message: 'API is running...'});
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
