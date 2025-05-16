37184bd61a72f2051fd154570c92f139799e6f73
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2025-05-16 12:48:22 +0530

    changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -77,8 +77,10 @@ app.use(cookieParser());
  * across different domains/ports
  */
 app.use(cors({
-    origin:  "https://car-rent-com.vercel.app", 
-    credentials: true, 
+    origin: process.env.ISPRODUCTION ? 'car-rent-cpbwtu35q-ujjwals-projects-76fd4110.vercel.app'  :["http://localhost:5500"],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }));
 
 /**
--- frontend/src/services/Api.service.js
+++ frontend/src/services/Api.service.js
@@ -1,3 +1,4 @@
 angular.module('myApp').service('ApiService', function() {
-    this.baseURL = 'https://carrent-com-7.onrender.com'; // Base URL of the Backend API
+    const ISPRODUCTION = false; // Flag to indicate if the app is in production mode
+    this.baseURL = ISPRODUCTION ? 'car-rent-cpbwtu35q-ujjwals-projects-76fd4110.vercel.app' : 'http://localhost:8000'; // Base URL of the Backend API
 });
