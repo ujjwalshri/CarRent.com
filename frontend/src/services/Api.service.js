96667354278b75c137a5c9072a97df243d4d7938
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2025-05-16 12:57:08 +0530

    changed

--- frontend/src/services/Api.service.js
+++ frontend/src/services/Api.service.js
@@ -1,4 +1,4 @@
 angular.module('myApp').service('ApiService', function() {
-    const ISPRODUCTION = window.location.hostname !== 'localhost';
+    const ISPRODUCTION = true;
     this.baseURL = ISPRODUCTION ? 'https://carrent-com-7.onrender.com' : 'http://localhost:8000';
 });
