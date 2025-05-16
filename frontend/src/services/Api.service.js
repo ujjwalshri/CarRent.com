39d57666db7e968bdf87f6388398dc469fedb1e2
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2025-05-16 12:49:36 +0530

    changed

--- frontend/src/services/Api.service.js
+++ frontend/src/services/Api.service.js
@@ -1,4 +1,4 @@
 angular.module('myApp').service('ApiService', function() {
     const ISPRODUCTION = false; // Flag to indicate if the app is in production mode
-    this.baseURL = ISPRODUCTION ? 'car-rent-cpbwtu35q-ujjwals-projects-76fd4110.vercel.app' : 'http://localhost:8000'; // Base URL of the Backend API
+    this.baseURL = ISPRODUCTION ? 'https://car-rent-cpbwtu35q-ujjwals-projects-76fd4110.vercel.app/' : 'http://localhost:8000'; // Base URL of the Backend API
 });
